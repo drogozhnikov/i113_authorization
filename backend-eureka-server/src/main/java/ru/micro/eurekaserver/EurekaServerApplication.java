257adfe6c9e70e174ca858e410408e2a519d35d1
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-30 11:24:32 +0300

    Added: eureka server

---
+++ backend-eureka-server/src/main/java/ru/micro/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package ru.micro.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@CrossOrigin
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
--- backend-gateway/src/main/java/ru/micro/gateway/BackendGatewayApplication.java
+++ backend-gateway/src/main/java/ru/micro/gateway/BackendGatewayApplication.java
@@ -3,8 +3,10 @@ package ru.micro.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
+@CrossOrigin
 public class BackendGatewayApplication {
 
     public static void main(String[] args) {
